de7f9d2504de9e45080b1546404c74b854468cca
Pietro Nencini <nencinipietro2006@gmail.com>
2024-10-12 11:30:04 +0200

    Update index.php
    Still not working for the 2nd video

--- index.php
+++ index.php
@@ -9,21 +9,21 @@
     <?php
 
         $video1 = array("title" => "Me at the zoo", "id" => "jNQXAC9IVRw");
-        $video2 = array("title"=> "10 HOURS OF SPONGEBOB DANCING", "id" => "dMXJHw2z8s4");
+        $video2 = array("title"=> "Gandalf Sax Guy 10 Hours (Original)", "id" => "Sagg08DrO5U");
         $video3 = array("title"=> "MINECRAFT SPEEDRUN WORLD RECORD (7:01)", "id"=> "E3t24Urba6Y");
 
         $myVideos = [$video1, $video2, $video3];
 
         function insertYTVideo($vid, $larPerc, $altPx) {
             $out_vid = "<h2> ". $vid["title"] ." </h2>";
-            $out_vid .= "<iframe src='https://www.youtube.com/embed/".$vid["id"]."' style='width: {$larPerc}%; hight: {$altPx}px; border: none;' title='iframe_video' </iframe> ";
+            $out_vid .= "<iframe src='https://www.youtube.com/embed/".$vid["id"]."' style='width: {$larPerc}%; height: {$altPx}px; border: none;' title='iframe_video' </iframe> ";
             return $out_vid;
         }
 
         $init_lar = 50;
         $init_alt = 250;
-        foreach($myVideos as $v) {
-            echo insertYTVideo($v, $init_lar, $init_alt);
+        for(/*$myVideos as $v*/ $i = 0; $i < count($myVideos); $i++) {
+            echo insertYTVideo($myVideos[$i], $init_lar, $init_alt);
             $init_lar +=25;
             $init_alt += 125;
         }
